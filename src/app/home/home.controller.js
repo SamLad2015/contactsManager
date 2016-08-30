b3b978f6203460eb442b54202c4c3040566e4dd4
Sam Lad <samlad@sams-mac.home>
2016-08-30 14:19:41 +0100

    added ability to add new contact

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -9,12 +9,36 @@
     function HomeCtrl($scope) {
         var vm = this;
         vm.contacts = [];
+        vm.showAddEdit = false;
+        vm.addNewContact = addNewContact;
+        vm.submitNewContact = submitNewContact;
+        generateTemContact();
         activate();
 
         function activate() {
-            vm.contacts.push({id: 1, firstName: 'Tom', lastName: 'Noname' });
-            vm.contacts.push({id: 2,  firstName: 'John', lastName: 'Doe' });
-            vm.contacts.push({id: 3,  firstName: 'Mary', lastName: 'Moe' });
+            vm.contacts.push({ id: 1, firstName: 'Tom', lastName: 'Noname', phoneNumber: '100' });
+            vm.contacts.push({ id: 2, firstName: 'John', lastName: 'Doe', phoneNumber: '200' });
+            vm.contacts.push({ id: 3, firstName: 'Mary', lastName: 'Moe', phoneNumber: '300' });
+        }
+
+        function addNewContact() {
+            vm.showAddEdit = true;
+        }
+
+        function generateTemContact() {
+            vm.tempContact = {
+                id: _.max(vm.contacts, function (contact) {
+                    return contact.id;
+                }),
+                firstName: '',
+                lastName: '',
+                phoneNumber: ''
+            };
+        }
+
+        function submitNewContact() {
+            vm.contacts.push(vm.tempContact);
+            generateTemContact();
         }
     }
 })();
